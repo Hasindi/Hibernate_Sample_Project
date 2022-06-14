381379478b149de75b0044697c3b87f46299e78b
hasindi <hmudithya@gmail.com>
2022-06-14 14:47:27 +0530

    Hibernate Sample Project

---
+++ src/AppInitializer.java
@@ -0,0 +1,47 @@
+import IK.hibernate.entity.Customer;
+import IK.hibernate.util.FactoryConfiguration;
+import javafx.application.Application;
+import javafx.stage.Stage;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+public class AppInitializer  {
+
+    public static void main(String[] args) {
+
+        Customer c1 = new Customer();
+        c1.setId("C001");
+        c1.setName("Kamal");
+        c1.setSalary(25000);
+        c1.setAddress("Galle");
+
+       /* Customer c2 = new Customer();
+        c2.setId("C002");
+        c2.setName("Nuwan");
+        c2.setSalary(25000);
+        c2.setAddress("Galle");
+
+        Customer c3 = new Customer();
+        c3.setId("C003");
+        c3.setName("Gayan");
+        c3.setSalary(25000);
+        c3.setAddress("Galle");
+
+        Customer c4 = new Customer();
+        c4.setId("C004");
+        c4.setName("Pawan");
+        c4.setSalary(25000);
+        c4.setAddress("Galle");*/
+
+
+        Session session = FactoryConfiguration.getInstance().getSession();
+
+        Transaction transaction = session.beginTransaction();
+        session.save(c1);
+
+        transaction.commit();
+
+        session.close();
+    }
+
+}
