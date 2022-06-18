d0acad542d3f77677f91a136f1dc4e60d90f5875
hasindi <hmudithya@gmail.com>
2022-06-18 13:19:40 +0530

    Create Encapsulatin class and pluged Lombox

---
+++ src/IK/hibernate/entity/Encapsulation.java
@@ -0,0 +1,14 @@
+package IK.hibernate.entity;
+
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+//@Setter
+//@Getter
+//@ToString
+@Data//getter, setter, toString, Hashmap
+public class Encapsulation {
+    private String id;
+    private String name;
+}
